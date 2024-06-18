4aca729919f5343e0323b8c7051e5f99ff8fb3d0
Steven Calvert <sdc977@gmail.com>
2024-06-17 23:26:05 -0700

    Basic weapon scene and animations

--- player/player.gd
+++ player/player.gd
@@ -4,13 +4,26 @@ class_name Player
 
 signal hpChange
 
+class_name Player
 var input_movement = Vector2.ZERO
 
-@export var stats : Resource
+@export var stats: Resource
 
 @onready var animations = $AnimationPlayer
-@onready var current_Hp : int = stats.max_Hp
-	
+@onready var weapon = $weapon
+
+var lastAnimDirection: String = "Down"
+var isAttacking: bool = false
+
+signal hpChange
+
+var input_movement = Vector2.ZERO
+
+@export var stats: Resource
+
+@onready var animations = $AnimationPlayer
+@onready var current_Hp: int = stats.max_Hp
+
 func handleInput():
 	input_movement = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	
@@ -18,34 +31,50 @@ func handleInput():
 		velocity = input_movement * stats.spd
 	else:
 		velocity = Vector2.ZERO
+	
+	if Input.is_action_just_pressed("attack"):
+		attack()
+
+func attack():
+	animations.play("attack" + lastAnimDirection)
+	isAttacking = true
+	weapon.visible = true
+	await animations.animation_finished
+	isAttacking = false
+	weapon.visible = false
 		
 func updateAnimation():
+	if isAttacking: return
+	
 	if velocity.length() == 0:
 		if animations.is_playing():
 			animations.stop()
-			animations.seek(0.6,true)
+			animations.seek(0.6, true)
 	else:
 		var direction = "Down"
 		match [velocity.x, velocity.y]:
-			[var x, var _y] when x < 0:
+			[ var x, var _y]whenx < 0:
 				direction = "Left"
-			[var x, var _y] when x > 0:
+			[ var x, var _y]whenx > 0:
 				direction = "Right"
-			[var _x, var y] when y < 0:
+			[ var _x, var y]wheny < 0:
 				direction = "Up"
 			_:
-				direction="Down"
+				direction = "Down"
 		animations.play("move" + direction)
 
 func handleCollision():
 	for i in get_slide_collision_count():
 		var collision = get_slide_collision(i)
 		var collider = collision.get_collider()
-		if(collider.name == "slime" && current_Hp > 0):
+		if (collider.name == "slime"&&current_Hp > 0):
 			current_Hp -= 1
 			hpChange.emit()
 
 func _physics_process(_delta):
+		lastAnimDirection = direction
+	
+func _physics_process(delta):
 	handleInput()
 	move_and_slide()
 	handleCollision()
