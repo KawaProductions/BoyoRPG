1bddb7e725cdd03f2ba7118f99d31b06faab4ab5
Brian Corbita <btcorbita@gmail.com>
2024-07-12 15:31:17 -0700

    Friendly Fire Fix
    - Created enemy node group
      - Assigned slimes to enemy group
    - Changed collision detection to detect enemies rather than self

--- enemies/slime.gd
+++ enemies/slime.gd
@@ -57,7 +57,7 @@ func _on_detection_area_body_exited(_body):
 	player_chase = false
 
 func _on_hurt_box_area_entered(area):
-	if area.get_parent() == self:
+	if area.get_parent().is_in_group("enemy"):
 		return
 	animations.play("deathEffect")
 	disableCollisions()
