fef2c320e3c233046855d6293cd21d9d323d8908
Brian Corbita <47233439+Syvrus@users.noreply.github.com>
2024-08-23 14:22:26 -0700

    Commentary + Trim (#36)

--- UI/UIOverlay.gd
+++ UI/UIOverlay.gd
@@ -1,11 +1,12 @@
 extends CanvasLayer
-
+#Variable that reaches for objects outside of scope
 @export var player : Player
+#Initialize health bar UI
 @onready var healthBar = $HealthBar
-
+#Initialize UI
 func _ready():
-	player.hpChange.connect(update)
-	update()
-
+	player.hpChange.connect(update) #Waits for change in player health
+	update() #Change player health bar accordingly
+#Change UI based on player health
 func update():
 	healthBar.value = player.currentHealth * 100 / player.stats.max_Hp
--- enemies/slime.gd
+++ enemies/slime.gd
@@ -1,71 +1,74 @@
 extends CharacterBody2D
-
+#Variables that are able to be changed via inspector
 @export var stats: Resource
 @export var limit = 0.5
 @export var endPoint: Marker2D
-
+#Initialize animation tree
 @onready var animations = $AnimatedSprite2D
-
+#Patrol points
 var startPos
 var endPos
+#Flag for attack behavior
 var player_chase = false
-var player: CharacterBody2D = null
-var isDead: bool = false
-
+var player: CharacterBody2D = null #Behavior target
+var isDead: bool = false #Flag for entity death
+#Set patrol points on start
 func _ready():
-	startPos = position
-	endPos = endPoint.global_position
-
+	startPos = position #Spawn position
+	endPos = endPoint.global_position #End of path
+#Change of movement speed
 func updateVelocity():
 	var moveDirection
-	if player_chase:
+	if player_chase: #When chasing
 		moveDirection = player.position - position
-	else:
+	else: #When not chasing
 		moveDirection = endPos - position
-	if moveDirection.length() < limit:
+	if moveDirection.length() < limit: #When reached end point
 		changeDirection()
-	velocity = moveDirection.normalized() * stats.spd
-
+	velocity = moveDirection.normalized() * stats.spd #Set speed
+#Change animation
 func updateAnimation():
+	#When not moving
 	if velocity.length() == 0:
-		animations.stop()
+		animations.stop() #Stop playing moving animation
 		return
 		
-	var direction = "Down"
-	if velocity.y < 0:
-		direction = "Up"
-	
+	var direction = "Down" #Animation set to moving down
+	if velocity.y < 0: #When movement velocity is set upward
+		direction = "Up" #Animation set to moving up
+	#Play animation
 	animations.play("walk" + direction)
-	
+#Change direction
 func changeDirection():
+	#Swap beginning and ending positions
 	var tempEnd = endPos
 	endPos = startPos
 	startPos = tempEnd
-	
+#Game ticks
 func _physics_process(_delta):
-	if isDead: return
-	updateVelocity()
-	move_and_slide()
-	updateAnimation()
-
+	if isDead: return #Check if dead
+	updateVelocity() #Change movement speed
+	move_and_slide() #Move sprite
+	updateAnimation() #Change animation
+#When player enters detection radius
 func _on_detection_area_body_entered(body):
-	player = body
-	player_chase = true
-
+	player = body #Set player as targert
+	player_chase = true #Follow target
+#When player leaves detection radius
 func _on_detection_area_body_exited(_body):
-	player = null
-	player_chase = false
-
+	player = null #Remove player as target
+	player_chase = false #Do not follow target
+#Area on entity that the player can damage if hitbox comes into contact
 func _on_hurt_box_area_entered(area):
-	if area.get_parent().is_in_group("enemy"):
-		return
-	animations.play("deathEffect")
-	disableCollisions()
-	isDead = true
-	await animations.animation_finished
-	queue_free()
+	if area.get_parent().is_in_group("enemy"): #Checks if entity is an enemy
+		return #Does not take damage
+	animations.play("deathEffect") #Entity dies if in contact with player hitbox
+	disableCollisions() #Sprite no longer tangible
+	isDead = true #Sets to dead
+	await animations.animation_finished #finish death animation
+	queue_free() #Remove entity
 
-
+#Disables relavent areas
 func disableCollisions():
 	$CollisionShape2D.set_deferred("disabled", true)
 	$hitBox.set_deferred("monitorable", false)
